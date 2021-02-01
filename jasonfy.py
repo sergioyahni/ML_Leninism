f416939e9d29f9bf18ba8c57a2a9c684450f13bf
Sergio Yahni <sergioyahni@gmail.com>
2021-02-01 18:17:07 +0200

    Update jasonfy.py

--- jasonfy.py
+++ jasonfy.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 import json
 import random
 
@@ -5,7 +12,7 @@ import random
 import text file
 '''
 
-def create_dct(year, publication, txt, is_lenin):
+def create_dct(txt, is_lenin):
     lst = []
     nlst =[]
     f = open(txt)
@@ -22,19 +29,16 @@ def create_dct(year, publication, txt, is_lenin):
     
     for item in lst:
         dct = {}
-        dct['year'] = year
-        dct['publication'] = publication
         dct['statement'] = item
         dct['is_lenin'] = is_lenin
         nlst.append(dct)
     
     return nlst
 
-data = create_dct(1914, 'the_right_of_nations_to_self_determination', '1914_the_right_of_nations_to_self_determination', 1)
-data += create_dct(1916, 'Imperialism_the_highest_stage_of_capitalism', '1916_Imperialism_the_highest_stage_of_capitalism', 1)
-data += create_dct(1917, 'the_state_and_revolution', '1917_the_state_and_revolution', 1)
-data += create_dct(1776, 'the_wealth_of_nations', 'the_wealth_of_nations', 0)
+data = create_dct('is_lenin', 1)
+data += create_dct('not_lenin', 0)
+
 random.shuffle(data)
 
-with open('lenin_smith.json', 'w') as outfile:
+with open('lenin_dataset.json', 'w') as outfile:
     json.dump(data, outfile)
